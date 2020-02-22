24534c5101afac2ff4570f947515b976353b2224
Pauloa90 <paulo.a90@hotmail.com>
2020-02-22 17:21:57 +0000

    Displaying some data on the page

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+# Linking my app to MongoDB
+app.config["MONGO_DBNAME"] = 'reviewmanguezal'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-73r36.mongodb.net/reviewmanguezal?retryWrites=true&w=majority'
+
+# Creating a Instance for Pymongo
+mongo = PyMongo(app)
+
+@app.route("/manguezalclub")
+def manguezalclub():
+    return render_template('manguezalclub.html', manguezalclub=mongo.db.review.find())
+
 
 @app.route("/")
 def index():
