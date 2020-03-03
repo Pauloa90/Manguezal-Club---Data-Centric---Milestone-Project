f9d38b2408296a8aed281500b97376abab216805
Pauloa90 <paulo.a90@hotmail.com>
2020-03-03 10:07:44 +0000

    adding button add category

--- run.py
+++ run.py
@@ -76,6 +76,24 @@ def update_category(category_id):
         {'category_name': request.form.get['category_name']})
     return redirect(url_for('get_categories'))
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    redirect(url_for('get_categories'))
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get['category_name']}
+    categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+@app.route('add')
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
